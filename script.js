aefa73e0c37cbdb5972926a651cb05d6760246e3
SakshamAryal <sakshamaryal.uni@gmail.com>
2026-05-02 12:45:03 -0400

    need to fix calculations

--- script.js
+++ script.js
@@ -1,30 +1,88 @@
 const textbox = document.getElementById("display"); //works
 const numeric = Array.from(document.getElementsByClassName("numbers"));
-let currOperation;
+let prevOperation;
+let a, b;
+let reset = false, equals = false;
+lastAction = NaN;
 numeric.forEach((number) => {
   number.addEventListener("click", () => inputnum(number));
 });
+// handles input using the buttons
 function inputnum(number) {
+  if (equals){
+    clear
+  };
+  if (reset == true){
+    textbox.value = Number(number.textContent);
+    reset = false;
+    return;
+  };
   let newvalue = String(textbox.value);
   newvalue += String(number.textContent);
   textbox.value = Number(newvalue);
-}
+  lastAction = 'num';
+};
 const operations = Array.from(document.getElementsByClassName("operation"));
-let a;
 operations.forEach((operation) => {
-  operation.addEventListener("click", () => arithmetic(operation));
+  operation.addEventListener("click", () => process(operation));
 });
-function arithmetic(operation) {
-  if (currOperation == NaN) {
+// handles the operation and how the calculator reacts for arithmetic processes
+function process(operation) {
+  if (prevOperation !== prevOperation){
     a = textbox.value;
-    currOperation = operation.textContent;
-    return;
   }
-  if (currOperation === "x") {
-    b = textbox.value
-    a = product(a,b)
+  else {
+    if(!isNaN(a)){
+      b = textbox.value;
+    };
+  };
+  reset = true;
+  tempOperation = prevOperation;
+  if (operation.textContent == "="){
+    solve(operation.textContent);
+    equals = true;
   }
-}
-function product(a, b){
-  return a*b
-}
+  else {
+    solve(operation.textContent);
+    equals = false;
+    prevOperation = operation.textContent;
+  };
+};
+//takes care of all the arithmetic process
+function arithmetic(type){
+  switch (type){
+    case "+":
+      return a + b;
+    case "-":
+      return a - b;
+    case "÷":
+      if (b == 0){
+        clear
+        alert("Cannot divide by zero")
+      }
+      else{
+        return a/b;
+      };
+    case "x":
+      return a*b;
+  };
+};
+function solve(type){
+  if (equals & type != "="){
+    return
+  }
+  result = arithmetic(prevOperation)
+  textbox.value = result;
+  a = result;
+};
+function clear(){
+  a = NaN;
+  b = NaN;
+  prevOperation = NaN;
+  equals = false;
+  textbox.value = "";
+  lastAction = NaN;
+};
+function update_history(type, result){
+  
+};
